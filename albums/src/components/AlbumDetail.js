8a2ff7990fd3641d422d2997779248fdd67f9364
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-13 21:57:40 -0500

    Added Card and CardSection components

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,14 +1,20 @@
 //  Import libraries
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+//  Import components
+import Card from './Card';
+import CardSection from './CardSection';
 
 //  Create component (functional)
     //  Album information passed to component using props.album
 const AlbumDetail = (props) => {
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <Text>{props.album.title}</Text>
+      </CardSection>
+    </Card>
   );
 };
 
