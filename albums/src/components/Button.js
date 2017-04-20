2059aa91a60d2366195de4f36aa29505751a7c5d
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-19 23:49:55 -0500

    Applied styling to Button component

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -4,11 +4,37 @@ import { Text, TouchableOpacity } from 'react-native';
 
 //  Create component
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
   return (
-    <TouchableOpacity>
-      <Text>Click me!!!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Click me!!!
+      </Text>
     </TouchableOpacity>
   );
+};
+
+//  Styling for Button component
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',   //  Flexbox property, stretch to fill
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 //  Export component
