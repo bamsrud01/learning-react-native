2f609f318312954de36f5b3e420dee1c4b917a6d
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-25 23:41:18 -0500

    Reused components from albums project

---
+++ auth/src/components/Button.js
@@ -0,0 +1,41 @@
+//  Import libraries
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+//  Create component
+const Button = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+//  Styling for Button component
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',   //  Flexbox property, stretch to fill
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
+};
+
+//  Export component
+export default Button;
