209728aaa3c7d765a8b76e185cb11c18d1daa9eb
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-25 23:26:09 -0500

    Added code for Android device

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,1 +1,24 @@
 //  This file is for Android
+
+/*  MUST TEST IN ANDROID EMULATOR!!!  */
+
+//  Import libraries
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+//  Import album components
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+//  Create App component
+const App = () => (
+  //  Components must return a single top-level element
+  //  Use headerText prop to dynamically change content of <Header>
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
+);
+
+//  Render component to device
+AppRegistry.registerComponent('albums', () => App);
