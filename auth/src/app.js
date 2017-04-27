94cc3453c4644dd0bff9798552a5fbdb58fe5015
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-26 23:25:29 -0500

    Added app.js

---
+++ auth/src/app.js
@@ -0,0 +1,17 @@
+//  Import libraries
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+//  Create App component
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>An App!</Text>
+      </View>
+    );
+  }
+}
+
+//  Export App
+export default App;
