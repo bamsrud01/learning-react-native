b3bb9a986ee73e6c04c0642caee62c9187022310
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-30 19:47:21 -0500

    Created LoginForm component

---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,15 @@
+//  Import libraries
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+//  Create LoginForm component
+class LoginForm extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+//  Export component
+export default LoginForm;
