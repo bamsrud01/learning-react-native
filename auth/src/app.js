11547d585ec421d54b7780d0ce6f3ceb8036fd76
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-30 19:43:59 -0500

    Set up firebase configuration in client

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,13 +1,24 @@
 //  Import libraries
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 //  Import components from index.js
 import { Header } from './components/common';
 
-
 //  Create App component
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDjzZXnXMonB6eHEX9tOBapZmN99vLxSg4',
+      authDomain: 'authentication-babc2.firebaseapp.com',
+      databaseURL: 'https://authentication-babc2.firebaseio.com',
+      projectId: 'authentication-babc2',
+      storageBucket: 'authentication-babc2.appspot.com',
+      messagingSenderId: '445470958576'
+  });
+  }
+
   render() {
     return (
       <View>
