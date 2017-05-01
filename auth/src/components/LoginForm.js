e1880ccefe7a0cc1c03eb72409cf4dbad1a19cd7
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-30 20:03:29 -0500

    Inserted user input field

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,17 +1,29 @@
 //  Import libraries
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 
 //  Import components
 import { Button, Card, CardSection } from './common';
 
 //  Create LoginForm component
 class LoginForm extends Component {
+  state = { text: '' };
+
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 100 }}
+          />
+        </CardSection>
+
+        <CardSection>
+
+        </CardSection>
+
         <CardSection>
           <Button>
             Log In
