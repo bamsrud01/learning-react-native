52af10618ed853d96143109f698c1de85115eab6
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-06 22:59:03 -0500

    Worked on Spinner component

--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -1,13 +1,24 @@
 //  Import libraries
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 //  Create Spinner component
-const Spinner = () => {
+const Spinner = ({ spinSize }) => {
   return (
-    <View />
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={spinSize || 'large'} />
+    </View>
   );
-}
+};
+
+//  Create styles
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 //  Export component
 export { Spinner };
