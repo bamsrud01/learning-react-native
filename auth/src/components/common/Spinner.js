f456b7b495b39f07441c3a62275928b04089cc0c
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-06 22:53:21 -0500

    Created Spinner component

---
+++ auth/src/components/common/Spinner.js
@@ -0,0 +1,13 @@
+//  Import libraries
+import React from 'react';
+import { View } from 'react-native';
+
+//  Create Spinner component
+const Spinner = () => {
+  return (
+    <View />
+  );
+}
+
+//  Export component
+export { Spinner };
