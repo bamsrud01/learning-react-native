ba58e57832027a67b5a1a76a767022f44f2d9ba2
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-21 01:16:26 -0500

    Created LibraryList component

---
+++ techstack/src/components/LibraryList.js
@@ -0,0 +1,13 @@
+//  Import libraries
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+//  Create component
+class LibraryList extends Component {
+  render() {
+    return;
+  }
+}
+
+//  Export component using connect (returns a function)
+export default connect()(LibraryList);
