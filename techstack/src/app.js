dcaba4594949de01c3171ad02682c9c7827cc360
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-21 00:04:55 -0500

    Created and registerd app component

---
+++ techstack/src/app.js
@@ -0,0 +1,13 @@
+//  Import libraries
+import React from 'react';
+import { View } from 'react-native';
+
+//  Create App component
+const App = () => {
+  return (
+    <View />
+  );
+};
+
+//  Export component
+export default App;
