f7bdbc60df70df2c80a84a8f65bb1affffa61ca6
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-20 23:06:30 -0500

    Handled user login and logout with firebase

--- auth/src/app.js
+++ auth/src/app.js
@@ -4,11 +4,13 @@ import { View } from 'react-native';
 import firebase from 'firebase';
 
 //  Import components from index.js
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 //  Create App component
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDjzZXnXMonB6eHEX9tOBapZmN99vLxSg4',
@@ -17,14 +19,39 @@ class App extends Component {
       projectId: 'authentication-babc2',
       storageBucket: 'authentication-babc2.appspot.com',
       messagingSenderId: '445470958576'
-  });
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
