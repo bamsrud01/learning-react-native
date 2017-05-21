649968b6bced8fdc0bb570148ee8ec054c7d043b
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-21 00:57:37 -0500

    Added LibraryReducer

---
+++ techstack/src/reducers/LibraryReducer.js
@@ -0,0 +1,5 @@
+//  Import JSON file
+import data from './LibraryList.json';
+
+//  Create and export reducer
+export default () => data;
--- techstack/src/reducers/index.js
+++ techstack/src/reducers/index.js
@@ -1,6 +1,11 @@
 //  Import from redux
 import { combineReducers } from 'redux';
 
+//  Import reducers
+import LibraryReducer from './LibraryReducer';
+
 export default combineReducers({
-  libraries: () => []
+  //  libraries is a key in the store's state, with value from LibraryReducer
+  //  { libraries: [{ id, title, description }] }
+  libraries: LibraryReducer
 });
