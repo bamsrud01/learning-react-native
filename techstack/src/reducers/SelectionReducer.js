82a0425951e2c8dc66e09a52ce65c0c36de1454c
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-03 22:07:35 -0500

    Added more details to reducers

--- techstack/src/reducers/SelectionReducer.js
+++ techstack/src/reducers/SelectionReducer.js
@@ -1,5 +1,9 @@
 //  Create and export reducer
-export default (state, action) => {
-  console.log(action);
-  return null;
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      return state;
+  }
 };
