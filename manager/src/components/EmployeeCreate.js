dd1e39a435429c26b4e8cf8d152231d7cc182c89
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-15 21:42:43 -0500

    Improved scene navigation, added EmployeeCreate component

---
+++ manager/src/components/EmployeeCreate.js
@@ -0,0 +1,18 @@
+//  Import libraries
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+//  Import components
+
+//  Create components
+class EmployeeCreate extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employee Form</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeCreate;
