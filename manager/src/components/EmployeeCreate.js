c14a298ec8946f4206a0d673e4f9a3aab8ebd7a4
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-20 21:36:25 -0500

    Began employee creation form

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,16 +1,37 @@
 //  Import libraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 
 //  Import components
+import { Card, CardSection, Input, Button } from './common';
 
 //  Create components
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="111-555-1234"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
