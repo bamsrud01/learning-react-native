a2cadcd144d467d20f8c2d38129d7ccfabad77fb
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-23 11:12:41 -0500

    Added list view

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,6 +1,6 @@
 //  Import libraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 //  Import action
@@ -10,6 +10,20 @@ import { employeesFetch } from '../actions';
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
