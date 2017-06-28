840b2bc11a4ccd3ed023117789a9a6746eec9669
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-28 12:36:28 -0500

    Created confirm modal

---
+++ manager/src/components/common/Confirm.js
@@ -0,0 +1,15 @@
+//  Import libraries
+import React from 'react';
+import { Text, View, Modal } from 'react-native';
+
+//  Import components
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+//  Create component
+const Confirm = () => {
+
+}
+
+//  Export component
+export { Confirm };
