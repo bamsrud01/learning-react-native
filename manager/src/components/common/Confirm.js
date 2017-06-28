10ac3d5f0fada1295e8d4047dbef45952dd179b7
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-28 12:48:16 -0500

    Completed Confirm modal functionality

--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -7,9 +7,26 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 //  Create component
-const Confirm = () => {
-
-}
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
 
 //  Export component
 export { Confirm };
